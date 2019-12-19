707349fa6589c17888b9071e722a2ec17f433881
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-19 13:31:57 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,10 +13,11 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   
-    if birthday_kids <= 12
     birthday_kids.each do |kids_name, age|
+      if age <= 12
        puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+     else 
+       puts "You are too old for this."
     end 
-  end 
   
 end 
