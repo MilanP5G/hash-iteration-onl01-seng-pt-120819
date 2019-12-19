7d7d9910df26e26aaaa25614c504579a44f9e037
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-19 13:26:03 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,4 +11,11 @@ def happy_birthday(birthday_kids)
   end 
 end
 
-
+def age_appropriate_birthday(birthday_kids)
+  
+  if age <= 12 
+    birthday_kids.each do |kids_name, age|
+       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end 
+  
+end 
